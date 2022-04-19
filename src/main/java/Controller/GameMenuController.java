8012139f870e3d37bd3f4f4bd248153b50d16be2
smizanian <95073227+smizanian@users.noreply.github.com>
2022-04-19 17:25:02 +0430

    GameMenuController created

---
+++ src/main/java/Controller/GameMenuController.java
@@ -0,0 +1,12 @@
+package Controller;
+
+import Model.GameModel;
+
+public class GameMenuController {
+
+    private GameModel gameModel;
+
+    public GameMenuController(GameModel gameModel) {
+        this.gameModel = gameModel;
+    }
+}
