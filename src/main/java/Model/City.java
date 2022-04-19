841ede5ecefd99a7a7c96d051b1310bc4de1dc7c
smizanian <95073227+smizanian@users.noreply.github.com>
2022-04-19 18:48:02 +0430

    City Updated

--- src/main/java/Model/City.java
+++ src/main/java/Model/City.java
@@ -1,4 +1,112 @@
 package Model;
 
+import Database.GameDatabase;
+
+import java.util.ArrayList;
+
 public class City {
+
+    private String name;
+    private int power;
+    private int longRangePower;
+    private int range;
+    private int foodGeneratingRate;
+    private int goldGeneratingRate;
+    private int scienceGenerating;
+    private int productionGenerating;
+    private int timeToExtendBorders;
+    private int timeTopPopulate;
+    private ArrayList<Citizen> citizens;
+    //private ArrayList<Building> buildings;
+    //private boolean isGarrison;
+    //private boolean isDivision;
+    private int hitPoint;
+    private String civilizationName;
+    //private boolean isColonized;
+    private boolean isCapital;
+
+    public City(String name, int power, int foodGeneratingRate, int goldGeneratingRate, int scienceGenerating, int productionGenerating, int timeToExtendBorders, int timeTopPopulate, ArrayList<Citizen> citizens, String civilizationName, boolean isCapital) {
+        this.name = name;
+        this.power = power;
+        //this.longRangePower = 0;
+        //this.range = 0;
+        this.foodGeneratingRate = foodGeneratingRate;
+        this.goldGeneratingRate = goldGeneratingRate;
+        this.scienceGenerating = scienceGenerating;
+        this.productionGenerating = productionGenerating;
+        this.timeToExtendBorders = timeToExtendBorders;
+        this.timeTopPopulate = timeTopPopulate;
+        this.citizens = citizens;
+        //this.buildings = new ArrayList<Building>();
+        //this.hitPoint = 0;
+        this.civilizationName = civilizationName;
+        this.isCapital = isCapital;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public int getLongRangePower() {
+        return longRangePower;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public int getFoodGeneratingRate() {
+        return foodGeneratingRate;
+    }
+
+    public int getGoldGeneratingRate() {
+        return goldGeneratingRate;
+    }
+
+    public int getScienceGenerating() {
+        return scienceGenerating;
+    }
+
+    public int getProductionGenerating() {
+        return productionGenerating;
+    }
+
+    public int getTimeToExtendBorders() {
+        return timeToExtendBorders;
+    }
+
+    public int getTimeTopPopulate() {
+        return timeTopPopulate;
+    }
+
+    public ArrayList<Citizen> getCitizens() {
+        return citizens;
+    }
+
+    public int getHitPoint() {
+        return hitPoint;
+    }
+
+    public String getCivilizationName() {
+        return civilizationName;
+    }
+
+    public boolean isCapital() {
+        return isCapital;
+    }
+
+    public void generate() {
+        generateGold();
+    }
+
+    private void generateGold() {
+        Civilization civilization = GameDatabase.getCivilizationByName(this.civilizationName);
+        if(civilization != null) {
+            civilization.addGold(this.goldGeneratingRate);
+        }
+    }
 }
