74f53e352208b1c19e09bf801759aaeefbc0a755
AlirezaRM <alireza1909@yahoo.com>
2022-04-21 17:43:42 +0430

    printMap added

--- src/main/java/Database/GameDatabase.java
+++ src/main/java/Database/GameDatabase.java
@@ -1,12 +1,14 @@
 package Database;
 
 import Model.Civilization;
+import Model.Tile;
 
 import java.util.ArrayList;
 
 public class GameDatabase {
 
     public static ArrayList<Civilization> players;
+    public static ArrayList<Tile> map;
 
     public static void setPlayers(ArrayList<Civilization> players) {
         GameDatabase.players = players;
@@ -19,5 +21,24 @@ public class GameDatabase {
             }
         }
         return null;
+    }
+
+    public static Tile getBlockByXandY(int x,int y){
+        for (Tile tile : map) {
+            if (tile.getX()==x && tile.getY()==y) return tile;
+        }
+        return null;
+
+    }
+    public static void readMapFromFile(){
+
+    }
+
+    public static void writeMapOnTheFile(){
+
+    }
+
+    public static void generateMap(int numberOfPlayers){
+
     }
 }
