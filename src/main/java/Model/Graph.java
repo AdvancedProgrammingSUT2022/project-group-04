b4fe6d4a3c341af9df82fccac22fa3feb00fcbb1
Alireza-81 <alireza81.amiri@gmail.com>
2022-04-24 19:10:19 +0430

    are adjacent func added to graph setting edged is incomplete

--- src/main/java/Model/Graph.java
+++ src/main/java/Model/Graph.java
@@ -6,6 +6,27 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Graph {
+    public boolean areAdjacent(Tile tile1, Tile tile2){
+        if (tile1.getX() == tile2.getX() && (tile1.getY() + 1 == tile2.getY() || tile1.getY() - 1 == tile2.getY())) {
+            return true;
+        }
+        if (tile1.getX() - 1 == tile2.getX() && tile1.getY() == tile2.getY()){
+            return true;
+        }
+        if (tile1.getX() + 1 == tile2.getX() && (tile1.getY() + 1 == tile2.getY() || tile1.getY() - 1 == tile2.getY() || tile1.getY() == tile2.getY())){
+            return true;
+        }
+        return false;
+    }
+    public void setEdges(ArrayList<Tile> copyOfMap){
+        for (int i = 0; i < copyOfMap.size(); i++){
+            for (int j = i + 1; j < copyOfMap.size(); j++){
+                if (areAdjacent(copyOfMap.get(i), copyOfMap.get(j))){
+
+                }
+            }
+        }
+    }
     public void add_neighbor(Tile origin ,Tile neighbourTile){
         origin.getNeighbors().add(neighbourTile);
         neighbourTile.getNeighbors().add(origin);
