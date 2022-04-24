32b02c24b43b6b6c4c5fe4e44759f467bdc1f11c
AlirezaRM <alireza1909@yahoo.com>
2022-04-24 22:12:32 +0430

    Soldier Constructor completed

--- src/main/java/Model/Soldier.java
+++ src/main/java/Model/Soldier.java
@@ -1,111 +1,190 @@
 package Model;
 
-public class Soldier extends Unit{
+public class Soldier extends Unit {
     private int range;
     private String combatType;
+    private Technology requiredTechnology;
+    private Resources requiredResources;
+    private int rangedCombatStrength;
+    private int combatStrength;
 
     public Soldier(int x, int y, int vX, int vY, int power, int cost, int movementPoint, String unitType, boolean isSleeping, boolean isReady, String era, int HP) {
         super(x, y, vX, vY, power, cost, movementPoint, unitType, isSleeping, isReady, era, HP);
-        switch (unitType){
+        switch (unitType) {
             case "Archer":
-                this.range = ;
+                this.range = 2;
                 this.combatType = "Archery";
+                this.combatStrength = 4;
+                this.rangedCombatStrength = 6;
+                this.requiredTechnology = new Technology("Archery");
                 break;
             case "ChariotArcher":
-                this.range = ;
+                this.range = 4;
                 this.combatType = "Mounted";
+                this.combatStrength = 3;
+                this.rangedCombatStrength = 6;
+                this.requiredTechnology = new Technology("TheWheel");
+                this.requiredResources = new Resources("Horse");
                 break;
             case "Scout":
-                this.range = ;
+                this.range = 2;
                 this.combatType = "Recon";
+                this.combatStrength = 4;
+                this.rangedCombatStrength = 0;
                 break;
             case "Settler":
-                this.range = ;
+            case "Worker":
+                this.range = 2;
                 this.combatType = "Civilian";
+                this.combatStrength = 0;
+                this.rangedCombatStrength = 0;
                 break;
             case "Spearman":
-                this.range = ;
+                this.range = 2;
                 this.combatType = "Melee";
+                this.combatStrength = 7;
+                this.rangedCombatStrength = 0;
+                this.requiredTechnology = new Technology("BronzeWorking");
                 break;
             case "Warrior":
-                this.range = ;
+                this.range = 2;
                 this.combatType = "Archery";
-                break;
-            case "Worker":
-                this.range = ;
-                this.combatType = "Civilian";
+                this.combatStrength = 6;
+                this.rangedCombatStrength = 0;
                 break;
             case "Catapult":
-                this.range = ;
+                this.range = 2;
                 this.combatType = "Siege";
+                this.combatStrength = 4;
+                this.rangedCombatStrength = 14;
+                this.requiredTechnology = new Technology("Mathematics");
+                this.requiredResources = new Resources("Iron");
                 break;
             case "Horseman":
-                this.range = ;
+                this.range = 4;
                 this.combatType = "Mounted";
+                this.combatStrength = 12;
+                this.rangedCombatStrength = 0;
+                this.requiredTechnology = new Technology("HorsebackRiding");
+                this.requiredResources = new Resources("Horse");
                 break;
             case "Swordsman":
-                this.range = ;
+                this.range = 2;
                 this.combatType = "Melee";
+                this.combatStrength = 11;
+                this.rangedCombatStrength = 0;
+                this.requiredTechnology = new Technology("IronWorking");
+                this.requiredResources = new Resources("Iron");
                 break;
             case "Crossbowman":
-                this.range = ;
+                this.range = 2;
                 this.combatType = "Archery";
+                this.combatStrength = 6;
+                this.rangedCombatStrength = 12;
+                this.requiredTechnology = new Technology("Machinery");
                 break;
             case "Knight":
-                this.range = ;
+                this.range = 3;
                 this.combatType = "Mounted";
+                this.combatStrength = 18;
+                this.rangedCombatStrength = 0;
+                this.requiredTechnology = new Technology("Chivalry");
+                this.requiredResources = new Resources("Horse");
                 break;
             case "Longswordsman":
-                this.range = ;
+                this.range = 3;
                 this.combatType = "Melee";
+                this.combatStrength = 0;
+                this.rangedCombatStrength = 0;
+                this.requiredResources = new Resources("Iron");
+                this.requiredTechnology = new Technology("Steel");
                 break;
             case "Pikeman":
-                this.range = ;
+                this.range = 2;
                 this.combatType = "Melee";
+                this.combatStrength = 0;
+                this.rangedCombatStrength = 0;
+                this.requiredTechnology = new Technology("CivilService");
                 break;
             case "Trebuchet":
-                this.range = ;
+                this.range = 2;
                 this.combatType = "Siege";
+                this.combatStrength = 20;
+                this.rangedCombatStrength = 2;
+                this.requiredResources = new Resources("Iron");
+                this.requiredTechnology = new Technology("Physics");
                 break;
             case "Canon":
-                this.range = ;
+                this.range = 2;
                 this.combatType = "Siege";
+                this.combatStrength = 10;
+                this.rangedCombatStrength = 26;
+                this.requiredTechnology = new Technology("Chemistry");
                 break;
             case "Cavalry":
-                this.range = ;
+                this.range = 3;
                 this.combatType = "Mounted";
+                this.combatStrength = 25;
+                this.rangedCombatStrength = 0;
+                this.requiredResources = new Resources("Horse");
+                this.requiredTechnology = new Technology("MilitaryScience");
                 break;
             case "Lancer":
-                this.range = ;
+                this.range = 4;
                 this.combatType = "Mounted";
+                this.combatStrength = 22;
+                this.rangedCombatStrength = 0;
+                this.requiredResources = new Resources("Horse");
+                this.requiredTechnology = new Technology("Metallurgy");
                 break;
             case "Musketman":
-                this.range = ;
+                this.range = 2;
                 this.combatType = "Gunpowder";
+                this.combatStrength = 16;
+                this.rangedCombatStrength = 0;
+                this.requiredTechnology = new Technology("Gunpowder");
                 break;
             case "Rifleman":
-                this.range = ;
+                this.range = 2;
                 this.combatType = "Gunpowder";
+                this.combatStrength = 25;
+                this.rangedCombatStrength = 0;
+                this.requiredTechnology = new Technology("Rifling");
                 break;
             case "AntiTankGun":
-                this.range = ;
+                this.range = 2;
                 this.combatType = "Gunpowder";
+                this.combatStrength = 32;
+                this.rangedCombatStrength = 0;
+                this.requiredTechnology = new Technology("ReplaceableParts");
                 break;
             case "Artillery":
-                this.range = ;
+                this.range = 2;
                 this.combatType = "Siege";
+                this.combatStrength = 16;
+                this.rangedCombatStrength = 32;
+                this.requiredTechnology = new Technology("Dynamite");
                 break;
             case "Infantry":
-                this.range = ;
+                this.range = 2;
                 this.combatType = "Gunpowder";
+                this.combatStrength = 36;
+                this.rangedCombatStrength = 0;
+                this.requiredTechnology = new Technology("ReplaceableParts");
                 break;
             case "Panzer":
-                this.range = ;
+                this.range = 5;
                 this.combatType = "Armored";
+                this.combatStrength = 60;
+                this.rangedCombatStrength = 0;
+                this.requiredTechnology = new Technology("Combustion");
                 break;
             case "Tank":
-                this.range = ;
+                this.range = 4;
                 this.combatType = "Armored";
+                this.combatStrength = 50;
+                this.rangedCombatStrength = 0;
+                this.requiredTechnology = new Technology("Combustion");
                 break;
             default:
                 break;
