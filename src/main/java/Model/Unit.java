fb25ac75b4a0e44ec9cca8bd4744c7d90670078e
Alireza-81 <alireza81.amiri@gmail.com>
2022-04-25 16:55:19 +0430

    removed is impossible to move function form unit class

--- src/main/java/Model/Unit.java
+++ src/main/java/Model/Unit.java
@@ -240,24 +240,4 @@ public class Unit {
         }
         return true;
     }
-
-    public boolean isImpossibleToMove(Tile currentTile, ArrayList<Tile> listOfCheckedTiles) {
-        ArrayList<Tile> adjacentTiles = new ArrayList<>();
-        adjacentTiles.add(GameDatabase.getTileByXAndY(currentTile.getX() - 1, currentTile.getY()));
-        adjacentTiles.add(GameDatabase.getTileByXAndY(currentTile.getX(), currentTile.getY() + 1));
-        adjacentTiles.add(GameDatabase.getTileByXAndY(currentTile.getX() + 1, currentTile.getY() + 1));
-        adjacentTiles.add(GameDatabase.getTileByXAndY(currentTile.getX() + 1, currentTile.getY()));
-        adjacentTiles.add(GameDatabase.getTileByXAndY(currentTile.getX() + 1, currentTile.getY() - 1));
-        adjacentTiles.add(GameDatabase.getTileByXAndY(currentTile.getX(), currentTile.getY() - 1));
-        int i = 0;
-        for (Tile tile : adjacentTiles) {
-            if (tile.canBePassed() && !listOfCheckedTiles.contains(tile) && !currentTile.isRiverByNumberOfEdge(i)) {
-                return true;
-            }
-            i++;
-        }
-        return false;
-    }
-
-
 }
