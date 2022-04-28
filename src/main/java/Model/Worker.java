e6b30463bb93a47b17a3943b2a3d8a3f18b70da0
AlirezaRM <alireza1909@yahoo.com>
2022-04-28 11:14:42 +0430

    Worker improving

--- src/main/java/Model/Worker.java
+++ src/main/java/Model/Worker.java
@@ -9,7 +9,7 @@ public class Worker extends Citizen{
     public Worker(int x, int y, int Vx, int Vy, int power, int cost, int movementPoint, String unitType, boolean isSleeping, boolean isReady, String era, int HP, int civilizationIndex){
         super(x, y, Vx, Vy, power, cost, movementPoint, unitType, isSleeping, isReady, era, HP, civilizationIndex);
     }
-    @Override
+    //@Override
     public void nextTurn(){
         Tile tile = GameDatabase.getTileByXAndY(this.x,this.y);
         int roundsTillFinishProject = tile.getRoundsTillFinishProjectByIndex(indexOfProject);
@@ -25,7 +25,6 @@ public class Worker extends Citizen{
     public void makeRoad(){
         Tile tile = GameDatabase.getTileByXAndY(this.x,this.y);
         Civilization civilization = GameDatabase.getCivilizationByTile(tile);
-        if ()
         if (civilization.isTechnologyInCivilization("Wheel")
                 && !tile.hasRoad
                 && !tile.getBaseTerrain().equals("Ice")
