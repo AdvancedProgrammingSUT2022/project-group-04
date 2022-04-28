0abc89c325e3603691d8b5d225fc1c973ff654a4
AlirezaRM <alireza1909@yahoo.com>
2022-04-28 10:28:20 +0430

    blah

--- src/main/java/Model/Worker.java
+++ src/main/java/Model/Worker.java
@@ -1,8 +1,26 @@
 package Model;
 
 public class Worker extends Citizen{
+    private boolean isAssigned;
+    private int roundsTillFinishProject;
+    private String typeOfWork;
     public Worker(int x, int y, int Vx, int Vy, int power, int cost, int movementPoint, String unitType, boolean isSleeping, boolean isReady, String era, int HP, int civilizationIndex){
         super(x, y, Vx, Vy, power, cost, movementPoint, unitType, isSleeping, isReady, era, HP, civilizationIndex);
     }
-    
+    @Override
+    public void nextTurn(){
+        roundsTillFinishProject -- ;
+        if (roundsTillFinishProject <= 0){
+            roundsTillFinishProject = 0;
+            isAssigned = false;
+            typeOfWork = "";
+        }
+    }
+    public void makeRoad(){
+        if ()
+        isAssigned = true;
+        typeOfWork = "Road";
+
+    }
+    public void
 }
