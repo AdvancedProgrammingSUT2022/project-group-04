70c8ae8e8d04496889c2b43a727f67b1e350e113
smizanian <s.mizanian@gmail.com>
2022-05-31 16:35:25 +0430

    Avatars added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,7 @@
 
      opens Civilization.Database to com.google.gson;
      opens Civilization.Model to com.google.gson;
+     opens Civilization.View.Components to com.google.gson;
 
      exports Civilization.View.FXMLControllers;
      opens Civilization.View.FXMLControllers to javafx.fxml;
