02d866044b39886a0e79089b82148dc1360f5a5d
AlirezaRM <alireza1909@yahoo.com>
2022-07-17 10:51:45 +0430

    server logic

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -7,11 +7,8 @@ import java.net.Socket;
 
 public class Client {
     public static Socket socket1;
-    public static Socket socket2;
     public static DataInputStream dataInputStream1;
     public static DataOutputStream dataOutputStream1;
-    public static DataInputStream dataInputStream2;
-    public static DataOutputStream dataOutputStream2;
 
     public static void setupSocketConnectionRegister(){
         try{
@@ -21,15 +18,6 @@ public class Client {
         } catch (IOException e){
             System.out.println("Connection lost");
             System.exit(5);
-        }
-    }
-    public static void setupSocketConnectionLogin(){
-        try{
-            socket2 = new Socket("localhost", 9000);
-            dataInputStream2 = new DataInputStream(socket2.getInputStream());
-            dataOutputStream2 = new DataOutputStream(socket2.getOutputStream());
-        } catch (IOException e){
-            e.printStackTrace();
         }
     }
 
