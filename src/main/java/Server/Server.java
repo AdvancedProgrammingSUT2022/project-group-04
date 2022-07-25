882af48d8b63c465d9ca8c8b1223b1b19cba9135
smizanian <s.mizanian@gmail.com>
2022-07-25 08:40:48 +0430

    karet ro bokon amiri

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -39,7 +39,6 @@ public class Server {
         Account.readAccounts("AccountURLs.json");
         Friendship.readFriendships("friendshipDatabase.json");
         Invitation.readInvitations("invitationDatabase.json");
-        Message.readMessages("chatDatabase.json");
         LoginMenuController loginMenuController = new LoginMenuController(new LoginMenuModel());
         ProfileMenuController profileMenuController = new ProfileMenuController(new ProfileMenuModel());
         try {
